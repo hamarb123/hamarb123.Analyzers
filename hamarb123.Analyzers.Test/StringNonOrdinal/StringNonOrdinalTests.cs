1e60295d401ee9b967da4568bd981dc7c02d7277
Hamish Arblaster <hamarb123@gmail.com>
2025-01-15 21:21:50 +1100

    Update StringNonOrdinalTests.cs

--- hamarb123.Analyzers.Test/StringNonOrdinal/StringNonOrdinalTests.cs
+++ hamarb123.Analyzers.Test/StringNonOrdinal/StringNonOrdinalTests.cs
@@ -55,10 +55,10 @@ namespace hamarb123.Analyzers.Test.StringNonOrdinal
 			var expected0 = VerifyCS.Diagnostic("HAM0004").WithLocation(0).WithArguments("Compare");
 			var expected1 = VerifyCS.Diagnostic("HAM0004").WithLocation(1).WithArguments("CompareTo");
 			var expected2 = VerifyCS.Diagnostic("HAM0004").WithLocation(2).WithArguments("EndsWith");
-			var expected5 = VerifyCS.Diagnostic("HAM0004").WithLocation(3).WithArguments("IndexOf");
-			var expected6 = VerifyCS.Diagnostic("HAM0004").WithLocation(4).WithArguments("LastIndexOf");
-			var expected7 = VerifyCS.Diagnostic("HAM0004").WithLocation(5).WithArguments("StartsWith");
-			var expected8 = VerifyCS.Diagnostic("HAM0004").WithLocation(6).WithArguments("IndexOf");
+			var expected3 = VerifyCS.Diagnostic("HAM0004").WithLocation(3).WithArguments("IndexOf");
+			var expected4 = VerifyCS.Diagnostic("HAM0004").WithLocation(4).WithArguments("LastIndexOf");
+			var expected5 = VerifyCS.Diagnostic("HAM0004").WithLocation(5).WithArguments("StartsWith");
+			var expected6 = VerifyCS.Diagnostic("HAM0004").WithLocation(6).WithArguments("IndexOf");
 
 			await VerifyCS.VerifyAnalyzerAsync(source,
 				expected0, expected1, expected2, expected3,
@@ -107,10 +107,10 @@ namespace hamarb123.Analyzers.Test.StringNonOrdinal
 			var expected0 = VerifyVB.Diagnostic("HAM0004").WithLocation(0).WithArguments("Compare");
 			var expected1 = VerifyVB.Diagnostic("HAM0004").WithLocation(1).WithArguments("CompareTo");
 			var expected2 = VerifyVB.Diagnostic("HAM0004").WithLocation(2).WithArguments("EndsWith");
-			var expected5 = VerifyVB.Diagnostic("HAM0004").WithLocation(3).WithArguments("IndexOf");
-			var expected6 = VerifyVB.Diagnostic("HAM0004").WithLocation(4).WithArguments("LastIndexOf");
-			var expected7 = VerifyVB.Diagnostic("HAM0004").WithLocation(5).WithArguments("StartsWith");
-			var expected8 = VerifyVB.Diagnostic("HAM0004").WithLocation(6).WithArguments("IndexOf");
+			var expected3 = VerifyVB.Diagnostic("HAM0004").WithLocation(3).WithArguments("IndexOf");
+			var expected4 = VerifyVB.Diagnostic("HAM0004").WithLocation(4).WithArguments("LastIndexOf");
+			var expected5 = VerifyVB.Diagnostic("HAM0004").WithLocation(5).WithArguments("StartsWith");
+			var expected6 = VerifyVB.Diagnostic("HAM0004").WithLocation(6).WithArguments("IndexOf");
 
 			await VerifyVB.VerifyAnalyzerAsync(source,
 				expected0, expected1, expected2, expected3,
